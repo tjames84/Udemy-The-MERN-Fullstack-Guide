ca238c1d5302654c8f1b3ec81f1a18b777931b4c
Anthony James <tjames84@gmail.com>
2020-08-13 16:51:34 -0600

    starting setup, pages and routes. used react-router-dom.

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import Users from './user/pages/Users';
+import NewPlace from './places/pages/NewPlaces';
+function App() {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          <Users />
+        </Route>
+
+        <Route path="/places/new">
+          <NewPlace />
+        </Route>
+
+        <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
