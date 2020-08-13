325aea9c8ce7df60e1f01ca436c55e7153009918
Anthony James <tjames84@gmail.com>
2020-08-13 13:26:45 -0600

    React basics

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,15 +1,7 @@
 import React from 'react';
 
-import GoalList from './components/GoalList';
-import './App.css';
-
 const App = () => {
-  return (
-    <div className="course-goals">
-      <h2>Course Goals</h2>
-      <GoalList />
-    </div>
-  );
+  return <h1 title="This works!">Hi, <span>this</span> is ReactJS!</h1>;
 };
 
 export default App;
