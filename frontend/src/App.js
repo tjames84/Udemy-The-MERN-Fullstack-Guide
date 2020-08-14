42b699f27ec871ea18ddc6928e9aba937ddfba0a
Anthony James <tjames84@gmail.com>
2020-08-13 19:43:15 -0600

    added components to UserItem

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom';
 
-import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
 import Users from './user/pages/Users';
-import NewPlace from './places/pages/NewPlaces';
-function App() {
+import NewPlace from './places/pages/NewPlace';
+
+const App = () => {
   return (
     <Router>
       <Switch>
         <Route path="/" exact>
           <Users />
         </Route>
-
-        <Route path="/places/new">
+        <Route path="/places/new" exact>
           <NewPlace />
         </Route>
-
         <Redirect to="/" />
       </Switch>
     </Router>
   );
-}
+};
 
 export default App;
