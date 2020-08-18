2b922e03aed366eda26775511143fb61c2c2262d
Anthony James <tjames84@gmail.com>
2020-08-17 19:35:05 -0600

    handling Drawer State

--- frontend/src/shared/components/Navigation/SideDrawer.js
+++ frontend/src/shared/components/Navigation/SideDrawer.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactDom from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import './SideDrawer.css';
 
 const SideDrawer = props => {
   const content = <aside className="side-drawer">{props.children}</aside>;
 
-  return ReactDom.createPortal(content, document.getElementById('drawer-hook'));
+  return ReactDOM.createPortal(content, document.getElementById('drawer-hook'));
 };
 
 export default SideDrawer;
