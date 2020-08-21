58aced70d768d715157ab46bedc309a0a9e186a3
Anthony James <tjames84@gmail.com>
2020-08-21 11:45:55 -0600

    Sending req & res with nodejs

--- node-express/app.js
+++ node-express/app.js
@@ -1,13 +1,25 @@
-const fs = require('fs');
+const http = require("http");
 
-const userName = "Tony";
+const server = http.createServer((req, res) => {
+  console.log("INCOMING REQUEST");
+  console.log(req.method, req.url);
 
-fs.writeFile('user-data.txt', 'Name: ' + userName, (err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
+  if (req.method === "POST") {
+    let body = "";
+    req.on("end", () => {
+      const userName = body.split("=")[1];
+      res.end("<h1>" + userName + "</h1>");
+    });
 
-    console.log('WROTE FILE');
+    req.on("data", (chunk) => {
+      body += chunk;
+    });
+  } else {
+    res.setHeader("Content-Type", "text/html");
+    res.end(
+      '<form method="POST"><input type="text" name="username"><button type="submit">Create User</button></form>'
+    );
+  }
 });
 
+server.listen(5000);
