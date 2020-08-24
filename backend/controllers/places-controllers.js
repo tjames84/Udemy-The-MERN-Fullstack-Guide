197f049236dcf1573713c807e9d5e2b0cd7c1ce5
Anthony James <tjames84@gmail.com>
2020-08-24 13:38:09 -0600

    validating Patch Requests & User Routes

--- backend/controllers/places-controllers.js
+++ backend/controllers/places-controllers.js
@@ -55,7 +55,7 @@ const createPlace = (req, res, next) => {
 
   if (!errors.isEmpty()) {
     console.log(errors);
-    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
   }
 
   const { title, description, coordinates, address, creator } = req.body;
@@ -75,6 +75,12 @@ const createPlace = (req, res, next) => {
 };
 
 const updatePlace = (req, res, next) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    console.log(errors);
+    throw new HttpError("Invalid inputs passed, please check your data.", 422);
+  }
   const { title, description } = req.body;
   const placeId = req.params.pid;
 
@@ -90,6 +96,9 @@ const updatePlace = (req, res, next) => {
 
 const deletePlace = (req, res, next) => {
   const placeId = req.params.pid;
+  if (!DUMMY_PLACES.find(p => p.id === placeId)) {
+    throw new HttpError('Could not find a place for that id.', 404);
+  }
   DUMMY_PLACES = DUMMY_PLACES.filter((p) => p.id !== placeId);
   res.status(200).json({ message: "Deleted place." });
 };
--- backend/routes/users-routes.js
+++ backend/routes/users-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const usersController = require('../controllers/users-controllers');
 
@@ -6,7 +7,13 @@ const router = express.Router();
 
 router.get('/', usersController.getUsers);
 
-router.post('/signup', usersController.signup);
+router.post(
+  "/signup",
+  check("name").not().isEmpty(),
+  check("email").normalizeEmail().isEmail(), // Test@test.com => test@test.com
+  check("password").isLength({min : 6}),
+  usersController.signup
+);
 
 router.post('/login', usersController.login);
 
