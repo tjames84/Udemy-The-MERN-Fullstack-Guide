d6a609e13321b13fe016b319fc3b2f2bae218ede
Anthony James <tjames84@gmail.com>
2020-08-24 10:38:36 -0600

    Added POST route and used postman to GET and POST

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,20 @@
-// Third party exports
 const express = require('express');
 const bodyParser = require('body-parser');
 
-// imports that you create
 const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
-app.use('/api/places', placesRoutes); // => /api/places/...
+app.use(bodyParser.json());
 
-// error handling middleware
+app.use('/api/places', placesRoutes); // => /api/places...
+
 app.use((error, req, res, next) => {
-    if (res.headerSent) {
-        return next(error);
-    }
-
-    res.status(error.code || 500);
-    res.json({ message: error.message || "An unknown error occurred!" });
+  if (res.headerSent) {
+    return next(error);
+  }
+  res.status(error.code || 500)
+  res.json({message: error.message || 'An unknown error occurred!'});
 });
 
 app.listen(5000);
--- backend/models/http-error.js
+++ backend/models/http-error.js
@@ -1,17 +1,8 @@
-/** 
- * References
- * 
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes/constructor
- * 
- * 
- */
-
 class HttpError extends Error {
-    constructor(message, errorCode) {
-        super(message); // Add a 'message' property
-        this.code = errorCode; // adds a "code" property
-    }
+  constructor(message, errorCode) {
+    super(message); // Add a "message" property
+    this.code = errorCode; // Adds a "code" property
+  }
 }
 
 module.exports = HttpError;
